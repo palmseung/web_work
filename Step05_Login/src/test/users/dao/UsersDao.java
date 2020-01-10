006301e3e3c0b3f4623041c645dd55fb2a83cf41
LeeSeungHee <palmseung@gmail.com>
2020-01-10 15:58:42 +0900

    Step05_Login 기능 구현

--- Step05_Login/src/test/users/dao/UsersDao.java
+++ Step05_Login/src/test/users/dao/UsersDao.java
@@ -2,6 +2,7 @@ package test.users.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import test.users.dto.UsersDto;
 import test.util.DbcpBean;
@@ -16,6 +17,42 @@ public class UsersDao {
 			dao=new UsersDao();
 		}
 		return dao;
+	}
+	
+	//인자로 전달되는 UsersDto에 담긴 정보가 유효한 정보인지 여부를 리턴해주는 메소드
+	public boolean isValid(UsersDto dto) {
+		boolean isValid=false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "SELECT * FROM users"
+					+ " WHERE id=? AND pwd=?";
+			pstmt = conn.prepareStatement(sql);
+			// ? 에 값 바인딩 
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			
+			rs = pstmt.executeQuery();
+			while (rs.next()) {
+				isValid=true; //SELECT 된 row가 있어야 while 문이 수행된다.(즉 유효한 id, pwd 정보여야)
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				//connection pool 에 반납하기 
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return isValid;
 	}
 	
 	//회원 가입 정보를 DB에 저장하는 메소드
@@ -51,4 +88,6 @@ public class UsersDao {
 			return false;
 		}
 	}
+	
+	
 }
